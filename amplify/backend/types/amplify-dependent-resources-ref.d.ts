34a0b50adecd8e13aec7a0df925f23416739d7cc
DESKTOP-U775BCB\barte <bhalat@com4it.com>
2024-03-18 21:17:04 +0100

    authentication

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "davistestbe5c9a98": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import logo from "./logo.svg";
+import "@aws-amplify/ui-react/styles.css";
 import './App.css';
+import {
+  withAuthenticator,
+  Button,
+} from "@aws-amplify/ui-react";
 
-function App() {
+function App({ signOut }) {
   return (
     <div className="App">
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>Davis Tinder!</h1>
+        <Button onClick={signOut}>Sign Out</Button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
